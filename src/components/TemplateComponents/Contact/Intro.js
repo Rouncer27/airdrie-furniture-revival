b5a35b1aa5cd89930fbdbc173bf4e7a3570e2c49
Rouncer27 <trevor@switchbackcreative.ca>
2020-09-11 13:00:03 -0600

    connect page

---
+++ src/components/TemplateComponents/Contact/Intro.js
@@ -0,0 +1,128 @@
+import React from "react"
+import styled from "styled-components"
+import Img from "gatsby-image"
+import BGImage from "gatsby-background-image"
+import { medWrapper, colors, H1White, B1White } from "../../../styles/helpers"
+
+const IntroSection = styled.section`
+  position: relative;
+  margin-top: 5rem;
+  margin-bottom: 10rem;
+  z-index: 10;
+
+  .wrapper {
+    ${medWrapper};
+    position: relative;
+  }
+
+  .introTop {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    width: 100%;
+    margin-bottom: 2.5rem;
+
+    &__imageLeft {
+      width: calc(25% - 2.5rem);
+      min-height: 30rem;
+      margin-right: 2.5rem;
+
+      div {
+        width: 100%;
+        height: 100%;
+      }
+    }
+
+    &__content {
+      display: flex;
+      align-items: center;
+      flex-wrap: wrap;
+      justify-content: center;
+      width: 50%;
+      background-color: ${colors.colorTertiary};
+
+      &--inner {
+        width: 100%;
+        padding: 2rem 5rem;
+
+        h2 {
+          ${H1White};
+        }
+
+        p {
+          ${B1White};
+        }
+      }
+    }
+
+    &__imageRight {
+      width: calc(25% - 2.5rem);
+      min-height: 30rem;
+      margin-left: 2.5rem;
+
+      div {
+        width: 100%;
+        height: 100%;
+      }
+    }
+  }
+
+  .introLogo {
+    position: absolute;
+    bottom: -5rem;
+    left: 16.5%;
+    width: 20rem;
+    height: 20rem;
+  }
+`
+
+const Intro = ({ contactIntro }) => {
+  return (
+    <IntroSection>
+      <div className="wrapper">
+        <div className="introTop">
+          <div className="introTop__imageLeft">
+            <BGImage
+              tag="div"
+              fluid={
+                contactIntro.acf._afr_cpit_image_left.localFile.childImageSharp
+                  .fluid
+              }
+            />
+          </div>
+          <div className="introTop__content">
+            <div className="introTop__content--inner">
+              <div>
+                <h2>{contactIntro.acf._afr_cpit_title}</h2>
+              </div>
+              <div
+                dangerouslySetInnerHTML={{
+                  __html: contactIntro.acf._afr_cpit_content,
+                }}
+              />
+            </div>
+          </div>
+          <div className="introTop__imageRight">
+            <BGImage
+              tag="div"
+              fluid={
+                contactIntro.acf._afr_cpit_image_right.localFile.childImageSharp
+                  .fluid
+              }
+            />
+          </div>
+        </div>
+        <div className="introLogo">
+          <Img
+            fluid={
+              contactIntro.acf._afr_cpi_logo.localFile.childImageSharp.fluid
+            }
+            alt={contactIntro.acf._afr_cpi_logo.alt_text}
+          />
+        </div>
+      </div>
+    </IntroSection>
+  )
+}
+
+export default Intro
--- src/templates/contact.js
+++ src/templates/contact.js
@@ -1,14 +1,90 @@
 import React from "react"
+import { graphql } from "gatsby"
 
 import Layout from "../components/Layout"
 import SEO from "../components/Seo"
+import Intro from "../components/TemplateComponents/Contact/Intro"
+import Testimonials from "../components/TemplateComponents/Contact/Testimonials"
+import AsSeenOn from "../components/TemplateComponents/Contact/AsSeenOn"
 
-const contact = () => {
+const contact = props => {
+  const { contactIntro, testimonials, asSeenOn } = props.data
+  const location = props.location
   return (
     <Layout>
-      <h1>contact</h1>
+      <SEO title="content" />
+      <Intro contactIntro={contactIntro} />
+      <AsSeenOn asSeenOn={asSeenOn} />
+      <Testimonials testimonials={testimonials} />
     </Layout>
   )
 }
 
+export const connectQuery = graphql`
+  query connectPage($id: Int!) {
+    contactIntro: wordpressPage(wordpress_id: { eq: $id }) {
+      acf {
+        _afr_cpit_image_left {
+          alt_text
+          localFile {
+            childImageSharp {
+              fluid(maxWidth: 1000) {
+                ...GatsbyImageSharpFluid_withWebp
+              }
+            }
+          }
+        }
+        _afr_cpit_image_right {
+          alt_text
+          localFile {
+            childImageSharp {
+              fluid(maxWidth: 1000) {
+                ...GatsbyImageSharpFluid_withWebp
+              }
+            }
+          }
+        }
+        _afr_cpit_title
+        _afr_cpit_content
+        _afr_cpi_logo {
+          alt_text
+          localFile {
+            childImageSharp {
+              fluid(maxWidth: 500) {
+                ...GatsbyImageSharpFluid_withWebp
+              }
+            }
+          }
+        }
+      }
+    }
+
+    testimonials: allWordpressWpTestimonials {
+      edges {
+        node {
+          acf {
+            _afr_testimonial_content
+          }
+        }
+      }
+    }
+
+    asSeenOn: wordpressPage(wordpress_id: { eq: $id }) {
+      acf {
+        _afr_hoasnn_logos {
+          logo {
+            alt_text
+            localFile {
+              childImageSharp {
+                fluid(maxWidth: 400) {
+                  ...GatsbyImageSharpFluid_withWebp
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
 export default contact
