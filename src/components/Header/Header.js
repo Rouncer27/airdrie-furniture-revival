054766ff3639cef481ec0e15b2603d2e76263c69
Rouncer27 <trevor@switchbackcreative.ca>
2020-10-06 09:40:33 -0600

    mobile home page fixes

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useEffect } from "react"
 import styled from "styled-components"
 import HeaderNav from "./HeaderNav"
 import HeaderLogo from "./HeaderLogo"
@@ -6,6 +6,9 @@ import { medWrapper } from "../../styles/helpers"
 
 import Horse from "../ClosedCropped/Horse"
 import Flowers from "../ClosedCropped/Flowers"
+import { gsap } from "gsap"
+import { ScrollTrigger } from "gsap/ScrollTrigger"
+gsap.registerPlugin(ScrollTrigger)
 
 const HeaderStyled = styled.header`
   position: relative;
@@ -58,7 +61,7 @@ const HeaderStyled = styled.header`
   .flowersGraphic {
     display: none;
     position: absolute;
-    top: 0;
+    top: -1rem;
     right: 0;
     width: calc(46.14rem / 2);
     height: calc(29.35rem / 2);
@@ -71,20 +74,17 @@ const HeaderStyled = styled.header`
     }
 
     @media (min-width: 1025px) {
-      top: 0;
       right: 0;
       width: calc(46.14rem / 1.5);
       height: calc(29.35rem / 1.5);
     }
 
     @media (min-width: 1200px) {
-      top: 0;
       right: 0;
       width: calc(46.14rem / 1.2);
       height: calc(29.35rem / 1.2);
     }
     @media (min-width: 1500px) {
-      top: 0;
       right: 0;
       width: calc(46.14rem);
       height: calc(29.35rem);
@@ -93,8 +93,99 @@ const HeaderStyled = styled.header`
 `
 
 const Header = ({ siteTitle, location }) => {
+  useEffect(() => {
+    const triggerElement = document.querySelector("#headerSection")
+    const horseGraphic = triggerElement.querySelector(".horseGraphic")
+    const flowerGraphic = triggerElement.querySelector(".flowersGraphic")
+
+    gsap.set(horseGraphic, {
+      autoAlpha: 1,
+      y: -235,
+      x: 10,
+      rotate: "-30deg",
+      transformOrigin: "center top",
+    })
+    gsap.set(flowerGraphic, {
+      autoAlpha: 0,
+      y: 0,
+      x: 300,
+      transformOrigin: "center top",
+    })
+
+    gsap
+      .timeline({
+        scrollTrigger: {
+          trigger: triggerElement,
+          markers: false,
+          start: "top 100%",
+          toggleActions: "play none none none",
+        },
+      })
+      .add("start")
+      .to(
+        horseGraphic,
+        {
+          autoAlpha: 1,
+          y: -50,
+          x: 0,
+          rotate: "0deg",
+          duration: 2,
+          ease: "back.out(1.7)",
+        },
+        "start"
+      )
+      .to(horseGraphic, {
+        y: 0,
+        x: 0,
+        rotate: "0deg",
+        duration: 1.75,
+        ease: "power2.out",
+      })
+      .to(
+        flowerGraphic,
+        {
+          autoAlpha: 1,
+          y: 0,
+          x: 0,
+          duration: 1,
+          ease: "power2.out",
+        },
+        "start"
+      )
+
+    // gsap
+    //   .timeline({
+    //     scrollTrigger: {
+    //       trigger: triggerElement,
+    //       markers: true,
+    //       start: "top 0%",
+    //       end: "top -100%",
+    //       scrub: true,
+    //       toggleActions: "play none none none",
+    //     },
+    //   })
+    //   .add("start")
+    //   .to(
+    //     flowerGraphic,
+    //     {
+    //       x: 300,
+    //       duration: 1,
+    //       ease: "power2.out",
+    //     },
+    //     "start"
+    //   )
+    //   .to(
+    //     horseGraphic,
+    //     {
+    //       y: -235,
+    //       duration: 1,
+    //       ease: "power2.out",
+    //     },
+    //     "start"
+    //   )
+  }, [])
   return (
-    <HeaderStyled>
+    <HeaderStyled id="headerSection">
       <div className="headerWrap">
         <HeaderLogo siteTitle={siteTitle} />
         <HeaderNav location={location} />
